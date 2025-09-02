cc416d55f0dc2b0003413c89faa20dc734fdb1f6
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2025-09-02 19:19:04 +0200

    English version added

---
+++ final.js
@@ -0,0 +1,12 @@
+// const donkey = document.getElementById("donkey");
+// const train = document.getElementById("train");
+// function changeImages#() {
+//   donkey.classList.toggle("hide");
+//   train.classList.toggle("hide");
+// }
+
+// trainInterval = setInterval(changeImages, 3000);
+// clearInterval(myInterval);
+// function leaveApp() {
+//   clearInterval(trainInterval);
+// }
